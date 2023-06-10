a0413ea7671a62e7f6382fa2a2236caa61892eac
taewaun <taewaunkwon@gmail.com>
2023-06-10 14:01:01 +0900

    feat : practice Compose Components
    - Column, Row
    - Modifier + Arrangement + Alignment
    - Preview

--- app/src/main/java/com/twaun95/composeapp/MainActivity.kt
+++ app/src/main/java/com/twaun95/composeapp/MainActivity.kt
@@ -9,16 +9,24 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.twaun95.composeapp.ui.theme.ComposeAppTheme
+import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Conversation(messages = SampleData.conversationSample)
+                    Request2()
                 }
             }
         }
@@ -57,7 +65,7 @@ fun MessageCard(message: Message) {
                 .clip(CircleShape)
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
-        
+
         Spacer(modifier = Modifier.width(8.dp))
 
         Column() {
@@ -75,10 +83,60 @@ fun MessageCard(message: Message) {
     }
 }
 
+
+@Composable
+fun ColumnMessage(text1: String, text2: String) {
+    Column(
+        modifier = Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.End
+    ) {
+        Text(
+            text = text1,
+            color = MaterialTheme.colors.secondaryVariant,
+            style = MaterialTheme.typography.subtitle2
+        )
+        Text(
+            text = text2,
+            color = MaterialTheme.colors.secondaryVariant,
+            style = MaterialTheme.typography.subtitle2
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RequestPreview() {
+    ComposeAppTheme {
+        Request2()
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeAppTheme {
-        Conversation(messages = SampleData.conversationSample)
+        ColumnMessage(text1 = "hi", text2 = "android")
+    }
+}
+
+@Composable
+fun Request2() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "One Market",
+            color = Color.Black,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(10.dp),) {
+            Text(text = "로그인")
+        }
     }
 }
