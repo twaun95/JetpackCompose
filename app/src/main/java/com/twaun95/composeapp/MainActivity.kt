cd43eff551d1a9fa1729240c5a3494d943381b1f
taewaun <taewaunkwon@gmail.com>
2023-06-18 15:42:43 +0900

    feat : practice Compose Components
    - dialog
    - scaffold
    - radioButton

--- app/src/main/java/com/twaun95/composeapp/MainActivity.kt
+++ app/src/main/java/com/twaun95/composeapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.twaun95.composeapp
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -10,15 +13,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -107,18 +112,13 @@ fun ColumnMessage(text1: String, text2: String) {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun RequestPreview() {
-    ComposeAppTheme {
-        Request2()
-    }
+//    ComposeAppTheme {
+//        Request2()
+//        RequestAlertDialog()
+//        RequestScaffold()
+//    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeAppTheme {
-        ColumnMessage(text1 = "hi", text2 = "android")
-    }
-}
 
 @Composable
 fun Request2() {
@@ -135,8 +135,99 @@ fun Request2() {
         )
         Button(
             onClick = { /*TODO*/ },
-            shape = RoundedCornerShape(10.dp),) {
+            shape = RoundedCornerShape(10.dp),
+        ) {
             Text(text = "로그인")
         }
+    }
+}
+
+
+@Composable
+fun RequestAlertDialog() {
+    AlertDialog(
+        onDismissRequest = { /*TODO*/ },
+        confirmButton = {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "확인")
+            }
+        },
+        dismissButton = {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "취소")
+            }
+        },
+        title = {
+            Row {
+                Icon(
+                    imageVector = Icons.Default.Face,
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+                Text(text = "제목")
+            }
+        },
+        text = {
+            Text(text = "내용")
+        },
+    )
+}
+
+@Composable
+fun RequestScaffold() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "결제 수단 등록")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Outlined.ArrowBack, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+        ) {
+            Icon(
+                imageVector = Icons.Filled.CheckCircle,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(Alignment.Center),
+                tint = Color.Blue
+            )
+        }
+    }
+}
+
+@Composable
+fun TWRadioButton() {
+    Surface(
+        modifier = Modifier,
+//        shape = Shape,
+        border = BorderStroke(2.dp, Color.Gray)
+    ) {
+        Row {
+            RadioButton(
+                selected = true,
+                onClick = { /*TODO*/ }
+            )
+        }
+
+        Text(text = "Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TWRadioButtonPreview() {
+    ComposeAppTheme {
+        TWRadioButton()
     }
 }
